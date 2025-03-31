873dd1ee609314ca2d0076dc672d2663b7c76722
Peter <pk@LAPTOP-Q1U5JQU7.>
2025-03-31 16:16:48 +0200

    picked a name

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const PORT = process.env.PORT || 8080
 
 express()
     .get('/', (req, res) => {
-        res.send('Hello, Welcome to <Title TBD>"!');
+        res.send('Hello, Welcome to Taylor Industries!');
     })
     .listen(PORT, () => console.log(`Listening on ${PORT}`))
